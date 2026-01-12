e0c73cb46db673a2babb4dcb4866f577d68b81e6
Kevin Marques <kevinmarquesp478@protonmail.com>
2026-01-12 00:13:10 -0300

    Added a map to translate the enums to the PTBR names

--- app/index.js
+++ app/index.js
@@ -34,3 +34,28 @@ const PATTY = Object.freeze({
   REGULAR: "REGULAR",  // Carne 10:1
   CHICKEN: "CHICKEN",  // Carne de Chicken
 });
+
+/** To translate the meal enums to the portuguese names */
+const toPortuguese = Object.freeze({
+  [BREAD.REGULAR]: "Regular",
+  [BREAD.BIGMAC]: "Big Mac",
+  [BREAD.QUARTER]: "Quarterão",
+  [CONDIMENT.LETTUCE]: "Alface",
+  [CONDIMENT.PICLES]: "Picles",
+  [CONDIMENT.SLICED_CHEADDAR]: "Queijo Cheaddar",
+  [CONDIMENT.REIDRATED_ONION]: "Cebola Reidratada",
+  [CONDIMENT.FRESH_ONION]: "Cebola Fresca",
+  [CONDIMENT.SHOYO_ONION]: "Cebola Shoyo",
+  [SAUCE.BIGMAC]: "Molho Big Mac",
+  [SAUCE.KETCHUP]: "Ketchup",
+  [SAUCE.MUSTARD]: "Mostarda",
+  [SAUCE.MCMELT_CHEADDAR]: "Molho Cheadar McMelt",
+  [SAUCE.MAYONESE]: "Mayonese",
+  [SAUCE.MCMAYO]: "Méquinese",
+  [SAUCE.CBO]: "CBO",
+  [SAUCE.CREAM_RACH]: "Cream Ranch",
+  [SAUCE.TASTY]: "Molho Tasty",
+  [PATTY.QUARTER]: "Carne 4:1",
+  [PATTY.REGULAR]: "Carne 10:1",
+  [PATTY.CHICKEN]: "Carne de Chicken",
+});
