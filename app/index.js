2554d86b6e5e4618c3440dc6848c2e26e76e3f5a
Kevin Marques <kevinmarquesp478@protonmail.com>
2026-01-12 00:12:32 -0300

    Updated the meal items structure to simple enums

--- app/index.js
+++ app/index.js
@@ -1,41 +1,36 @@
-const INGREDIENTS = {
-  breads: [
-    { id: "regular",  name: "Regular" },
-    { id: "bigmac",   name: "Big Mac" },
-    { id: "quarter",  name: "Quarterão" },
-    { id: "dark_bun", name: "Pão Escuro (Integral)" },
-  ],
-  condiments: [
-    { id: "lettuce",         name: "Alface" },
-    { id: "picles",          name: "Picles" },
-    { id: "sliced_cheaddar", name: "Queijo Cheaddar" },
-    { id: "reidrated_onion", name: "Cebola Reidratada" },
-    { id: "fresh_onion",     name: "Cebola Fresca" },
-    { id: "shoyo_onion",     name: "Cebola Shoyo" },
-  ],
-  sauces: [
-    { id: "bigmac",          name: "Molho Big Mac" },
-    { id: "ketchup",         name: "Ketchup" },
-    { id: "mustard",         name: "Mostarda" },
-    { id: "mcmelt_cheaddar", name: "Molho Cheadar McMelt" },
-    { id: "mayonese",        name: "Mayonese" },
-    { id: "mcmayo",          name: "Méquinese" },
-    { id: "cbo",             name: "CBO" },
-    { id: "cream_rach",      name: "Cream Ranch" },
-    { id: "tasty",           name: "Molho Tasty" },
-  ],
-  patty: [
-    { id: "10:1",    name: "Carne 10:1" },
-    { id: "4:1",     name: "Carne 4:1 (Quarterão)" },
-    { id: "chicken", name: "Carne Chicken" },
-  ]
-};
+/** @enum {string} */
+const BREAD = Object.freeze({
+  REGULAR: "REGULAR",  // Regular
+  BIGMAC: "BIGMAC",    // Big Mac
+  QUARTER: "QUARTER",  // Quarterão
+});
 
-/*
-  Lista de metodos para um lanche
-    - Mostrar texto
-    - Remover condimento
-    - Trocar molho
-*/
+/** @enum {string} */
+const CONDIMENT = Object.freeze({
+  LETTUCE: "LETTUCE",                  // Alface
+  PICLES: "PICLES",                    // Picles
+  SLICED_CHEADDAR: "SLICED_CHEADDAR",  // Queijo Cheaddar
+  REIDRATED_ONION: "REIDRATED_ONION",  // Cebola Reidratada
+  FRESH_ONION: "FRESH_ONION",          // Cebola Fresca
+  SHOYO_ONION: "SHOYO_ONION",          // Cebola Shoyo
+});
 
-console.log(INGREDIENTS.sauces);
+/** @enum {string} */
+const SAUCE = Object.freeze({
+  BIGMAC: "BIGMAC",                    // Molho Big Mac
+  KETCHUP: "KETCHUP",                  // Ketchup
+  MUSTARD: "MUSTARD",                  // Mostarda
+  MCMELT_CHEADDAR: "MCMELT_CHEADDAR",  // Molho Cheadar McMelt
+  MAYONESE: "MAYONESE",                // Mayonese
+  MCMAYO: "MCMAYO",                    // Méquinese
+  CBO: "CBO",                          // CBO
+  CREAM_RACH: "CREAM_RACH",            // Cream Ranch
+  TASTY: "TASTY",                      // Molho Tasty
+});
+
+/** @enum {string} */
+const PATTY = Object.freeze({
+  QUARTER: "QUARTER",  // Carne 4:1
+  REGULAR: "REGULAR",  // Carne 10:1
+  CHICKEN: "CHICKEN",  // Carne de Chicken
+});
